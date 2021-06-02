87d6e0eabdd8baee295d17ddacabd1b332a55f12
Raman Karmakar <raman.karmakar@mobioapp.com>
2021-06-02 16:22:50 +0600

    Added API call for getting config data

--- app/src/main/java/com/datasoft/abs/presenter/view/dashboard/fragments/account/general/GeneralFragment.kt
+++ app/src/main/java/com/datasoft/abs/presenter/view/dashboard/fragments/account/general/GeneralFragment.kt
@@ -1,18 +1,22 @@
 package com.datasoft.abs.presenter.view.dashboard.fragments.account.general
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
 import com.datasoft.abs.databinding.GeneralFragmentBinding
+import com.datasoft.abs.presenter.utils.Resource
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class GeneralFragment : Fragment() {
 
-    private val viewModel: GeneralViewModel by activityViewModels()
+    private val viewModel: GeneralViewModel by viewModels()
     private var _binding: GeneralFragmentBinding? = null
 
     // This property is only valid between onCreateView and
@@ -24,16 +28,37 @@ class GeneralFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
         _binding = GeneralFragmentBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        return binding.root
+    }
 
-//        val textView: TextView = binding.textHome
-//        viewModel.getDashboardData().observe(viewLifecycleOwner, Observer {
-//            textView.text = it?.totalApplied.toString()
-//        })
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-        return root
+        val textView: TextView = binding.txtView
+        viewModel.getConfigData().observe(viewLifecycleOwner, { response ->
+
+            when(response) {
+                is Resource.Success -> {
+//                    goneProgressBar()
+                    response.data?.let { configResponse ->
+                        textView.text = configResponse.message
+                    }
+                }
+                is Resource.Error -> {
+//                    goneProgressBar()
+                    response.message?.let { message ->
+                        Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
+                        Log.e("TAG", "An error occurred: $message")
+                    }
+                }
+                is Resource.Loading -> {
+//                    showProgressBar()
+                }
+            }
+        })
+
+        viewModel.configData()
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/datasoft/abs/presenter/view/dashboard/fragments/account/general/GeneralViewModel.kt
+++ app/src/main/java/com/datasoft/abs/presenter/view/dashboard/fragments/account/general/GeneralViewModel.kt
@@ -1,11 +1,58 @@
 package com.datasoft.abs.presenter.view.dashboard.fragments.account.general
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.datasoft.abs.data.dto.config.ConfigResponse
 import com.datasoft.abs.domain.Repository
+import com.datasoft.abs.presenter.utils.Network
+import com.datasoft.abs.presenter.utils.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Response
 import javax.inject.Inject
 
 @HiltViewModel
-class GeneralViewModel @Inject constructor(private val repository: Repository): ViewModel() {
-    // TODO: Implement the ViewModel
+class GeneralViewModel @Inject constructor(
+    private val repository: Repository,
+    private val network: Network): ViewModel() {
+
+    private val configData = MutableLiveData<Resource<ConfigResponse>>()
+
+    fun getConfigData(): LiveData<Resource<ConfigResponse>> = configData
+
+    fun configData() {
+        viewModelScope.launch(Dispatchers.IO) {
+            if (network.isConnected()) {
+                try {
+                    val response = repository.getConfigData()
+                    configData.postValue(handleConfigResponse(response))
+                } catch (e: Exception) {
+                    configData.postValue(
+                        Resource.Error(
+                            "Something went wrong!", null
+                        )
+                    )
+                    e.printStackTrace()
+                }
+            } else {
+                configData.postValue(
+                    Resource.Error(
+                        "No internet connection", null
+                    )
+                )
+            }
+        }
+    }
+
+    private fun handleConfigResponse(response: Response<ConfigResponse>): Resource<ConfigResponse> {
+        if (response.isSuccessful) {
+            response.body()?.let { resultResponse ->
+                return Resource.Success(resultResponse)
+            }
+        }
+        return Resource.Error(response.message())
+    }
 }
