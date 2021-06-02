a574d119fb69750d182b9951f96e44eff09af77a
Raman Karmakar <raman.karmakar@mobioapp.com>
2021-06-02 17:19:40 +0600

    Image picker from both camera and gallery

--- app/src/main/java/com/datasoft/abs/presenter/view/dashboard/fragments/account/photo/PhotoFragment.kt
+++ app/src/main/java/com/datasoft/abs/presenter/view/dashboard/fragments/account/photo/PhotoFragment.kt
@@ -1,16 +1,18 @@
 package com.datasoft.abs.presenter.view.dashboard.fragments.account.photo
 
+import android.app.Activity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.activity.result.launch
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.datasoft.abs.databinding.PhotoFragmentBinding
 import com.datasoft.abs.presenter.utils.Photos
+import com.github.dhaval2404.imagepicker.ImagePicker
 import dagger.hilt.android.AndroidEntryPoint
 import java.util.*
 import javax.inject.Inject
@@ -42,13 +44,41 @@ class PhotoFragment : Fragment() {
         val root: View = binding.root
 
         binding.btnTakePhoto.setOnClickListener {
-            takePhoto.launch()
+//            takePhotoUsingCamera.launch()
+            ImagePicker.with(this)
+                .compress(1024)         //Final image size will be less than 1 MB(Optional)
+                .maxResultSize(1080, 1080)  //Final image resolution will be less than 1080 x 1080(Optional)
+                .createIntent { intent ->
+                    startForProfileImageResult.launch(intent)
+                }
+
         }
 
         return root
     }
 
-    private val takePhoto =
+    private val startForProfileImageResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+            val resultCode = result.resultCode
+            val data = result.data
+
+            when (resultCode) {
+                Activity.RESULT_OK -> {
+                    //Image Uri will not be null for RESULT_OK
+                    val fileUri = data?.data!!
+
+                    binding.imgView.setImageURI(fileUri)
+                }
+                ImagePicker.RESULT_ERROR -> {
+                    Toast.makeText(requireActivity(), ImagePicker.getError(data), Toast.LENGTH_SHORT).show()
+                }
+                else -> {
+                    Toast.makeText(requireActivity(), "Task Cancelled", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+    private val takePhotoUsingCamera =
         registerForActivityResult(ActivityResultContracts.TakePicturePreview()) {
             if (it == null)
                 return@registerForActivityResult
